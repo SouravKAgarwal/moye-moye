df8092f5ebbdfff8c9a499fc815baffa89b061e0
Sourav Kumar Agarwal <aggarwalsaurav948@gmail.com>
2023-12-31 17:22:16 +0530

    upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Gen from "./Gen";
+
+function App() {
+  return (
+    <>
+      <Gen />
+      <footer>
+        Made by
+        <div className="hover">Sourav and Bansal</div>
+      </footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Gen.jsx
@@ -0,0 +1,93 @@
+import { PDFDocument, rgb } from "pdf-lib";
+import { saveAs } from "file-saver";
+import fontkit from "@pdf-lib/fontkit";
+import { toast, Toaster } from "react-hot-toast";
+import { useState } from "react";
+
+const Gen = () => {
+  const [userName, setUserName] = useState("");
+  const fontUrl = "/DancingScript-Variable.ttf";
+
+  const capitalize = (str, lower = false) =>
+    (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, (match) =>
+      match.toUpperCase()
+    );
+
+  const audio = new Audio("../public/sound.mp3");
+
+  const generatePDF = async (name) => {
+    let nameLength = name.length;
+    if (nameLength > 30)
+      return alert("Name too long. Retry with a shorter name");
+    const fontBytes = await fetch(fontUrl).then((res) => res.arrayBuffer());
+    const existingPdfBytes = await fetch("./certificate1.pdf").then((res) =>
+      res.arrayBuffer()
+    );
+    const fontSize = nameLength > 20 ? 48 : 60;
+    const pdfDoc = await PDFDocument.load(existingPdfBytes);
+    pdfDoc.registerFontkit(fontkit);
+    const pages = pdfDoc.getPages();
+    const firstPage = pages[0];
+    const customFont = await pdfDoc.embedFont(fontBytes);
+    const pageSize = firstPage.getSize();
+    const textWidth = customFont.widthOfTextAtSize(name, fontSize);
+    const textHeight = customFont.heightAtSize(fontSize);
+    const pageWidth = pageSize.width;
+    const pageHeight = pageSize.height;
+
+    firstPage.drawText(name, {
+      x: pageWidth / 2 - textWidth / 2,
+      y: pageHeight / 2 - textHeight / 2,
+      size: fontSize,
+      font: customFont,
+      color: rgb(0.576, 0.463, 0.008),
+    });
+
+    const pdfBytes = await pdfDoc.save();
+    console.log("Done creating");
+
+    var file = new File([pdfBytes], "Certificate.pdf", {
+      type: "application/pdf;charset=utf-8",
+    });
+    saveAs(file);
+  };
+
+  const handleSubmit = () => {
+    const val = capitalize(userName);
+
+    if (val.trim() !== "") {
+      generatePDF(val);
+      audio.play();
+    } else {
+      toast.error("Enter your name!");
+    }
+  };
+
+  return (
+    <div className="Gen">
+      <h1>Certificate Generator</h1>
+      <input
+        type="text"
+        id="name"
+        placeholder="Enter your name"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <button id="submitBtn" onClick={handleSubmit}>
+        Generate PDF
+      </button>
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            padding: "16px",
+            color: "#ddd",
+            background: "#000",
+          },
+        }}
+      />
+    </div>
+  );
+};
+
+export default Gen;
